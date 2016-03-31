93c82c37dfe271032a0faf0e6b6dbc155d6764f5
rbs392 <rbs392@yahoo.com>
2016-03-31 16:39:59 +0530

    adding test for luthor
    need to remove hulk comparision

--- server/utils.js
+++ server/utils.js
@@ -22,17 +22,22 @@ var utils = {
 			if(/ERROR/.test(data)){
 				utils.logger("FETCH INCOMPLETE", url)
 			}
-			
-			if(/<html.*/.test(data)){
-				htmlPresent = true
-			}else if(/<\/html>/.test(data)){
-				htmlPresent = false
-				successdata += data
-			}
 
-			if(htmlPresent){
-				successdata += data
+			if(/<html><head><\/head><body><\/body><\/html>/.test(data)){
+				errordata += "Empty html recieved"
+			}else{
+				if(/<\/html>/.test(data)){
+					htmlPresent = false
+					successdata += data
+				}else if(/<html.*/.test(data)){
+					htmlPresent = true
+				}
+
+				if(htmlPresent){
+					successdata += data
+				}
 			}
+			
 		})
 
 		child.stderr.on("data", function(data){
@@ -43,7 +48,7 @@ var utils = {
 			if(errordata){
 				response.statusCode = 500
 				data = errordata
-				utils.logger("ERROR", errordata)
+				utils.logger("ERROR", errordata+ " : "+ url)
 			}else if(successdata){
 				response.statusCode = 200
 				data = successdata
